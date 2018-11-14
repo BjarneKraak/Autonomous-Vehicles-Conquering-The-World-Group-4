815d0da457631e1eacb1f5f63a124f7724ded625
BjarneKraak <bjarne.kraak@gmail.com>
2018-11-14 23:31:51 +0100

    Created speed control (1-10)
    Created a speed control for all four turning and driving functions. Those functions now have another parameter (by default 10 (max)) - speedfactor, which takes an int between 0 and 10. Because it's set by default, all old sketches should work. It has not been tested in Arduino. An problem that occurs is that the distance time to drive or turn for a certain distance/amount of degrees is not accurate anymore when speed_factor is not 10.

--- Libraries/Movement/Movement.cpp
+++ Libraries/Movement/Movement.cpp
@@ -15,10 +15,11 @@ void Movement::begin(int baudrate){ // begin
   }
 }
 
-void Movement::moveStraight(int distance, char drive_dir){
+void Movement::moveStraight(int distance, char drive_dir, int speed_factor){
+  int speed_time = findSpeedTime(speed_factor);
   float mv_time = (distance * 68); // calcute time to move depending on enterend distance
   if(drive_dir == 'f') { // if robot should move forward
-    maneuver(200, 200, mv_time); // move forward for mv_time ms
+    maneuver(speed_time, speed_time, mv_time); // move forward for mv_time ms
     maneuver(0, 0, 1); // stop moving
     if (_debug){ // if debug is on, print following:
       Serial.print("move forwards for ");
@@ -27,7 +28,7 @@ void Movement::moveStraight(int distance, char drive_dir){
     }
   }
   else if(drive_dir == 'b') { // if robot should move backwards
-    maneuver(-200, -200, mv_time); // move backwards for mv_time ms
+    maneuver(-speed_time, -speed_time, mv_time); // move backwards for mv_time ms
     maneuver(0, 0, 1); // stop moving
     if (_debug){ // if debug is on, print following:
       Serial.print("move backwards for ");
@@ -37,18 +38,11 @@ void Movement::moveStraight(int distance, char drive_dir){
   }
 }
 
-void Movement::maneuver(int speed_left, int speed_right, int ms_time){
-  // speedLeft, speedRight ranges: Backward  Linear  Stop  Linear   Forward
-  //                               -200      -100......0......100       200
-  servo_left.writeMicroseconds(1500 + speed_left);   // Set Left servo speed
-  servo_right.writeMicroseconds(1500 - speed_right); // Set right servo speed
-  delay(ms_time);                                   // Delay for msTime
-}
-
-void Movement::turn(int angle, char turn_dir){ // turning function, angle and direction needs to be entered
+void Movement::turn(int angle, char turn_dir, int speed_factor){ // turning function, angle and direction needs to be entered
+  int speed_time = findSpeedTime(speed_factor);
   int turn_time = ((angle * 6.0)); // variable that contains time to turn depended on angle
   if(turn_dir == 'r') { // if robot should turn right
-    maneuver(200, -200, turn_time); // turn right for turn_time ms
+    maneuver(speed_time, -speed_time, turn_time); // turn right for turn_time ms
     maneuver(0, 0, 1); // stop moving
     if (_debug){// if debug is on, print following:
       Serial.print("turn to the right for ");
@@ -57,7 +51,7 @@ void Movement::turn(int angle, char turn_dir){ // turning function, angle and di
     }
   }
   else if (turn_dir == 'l') { // if robot should turn left
-    maneuver(-200, 200, turn_time); // turn left for turn_time ms
+    maneuver(-speed_time, speed_time, turn_time); // turn left for turn_time ms
     maneuver(0, 0, 1); // stop moving
     if (_debug){// if debug is on, print following:
       Serial.print("turn to the left for ");
@@ -67,30 +61,32 @@ void Movement::turn(int angle, char turn_dir){ // turning function, angle and di
   }
 }
 
-void Movement::driveInf(char drive_dir){
+void Movement::driveInf(char drive_dir, int speed_factor){
+  int speed_time = findSpeedTime(speed_factor);
   if(drive_dir == 'f') { // if robot should move forward
-    maneuver(200, 200, 1); // move forward
+    maneuver(speed_time, speed_time, 1); // move forward
     if (_debug){// if debug is on, print following:
       Serial.println("move forwards until stop is called");
     }
   }
   else if(drive_dir == 'b') { // if robot should move backwards
-    maneuver(-200, -200, 1); // move backwards
+    maneuver(-speed_time, -speed_time, 1); // move backwards
     if (_debug){// if debug is on, print following:
       Serial.println("move backwards until stop is called");
     }
   }
 }
 
-void Movement::turnInf(char turn_dir){
+void Movement::turnInf(char turn_dir, int speed_factor){
+  int speed_time = findSpeedTime(speed_factor);
   if(turn_dir == 'r') { // if robot should turn right
-    maneuver(200, -200, 1); // turn right
+    maneuver(speed_time, -speed_time, 1); // turn right
     if (_debug){// if debug is on, print following:
       Serial.print("turn to the right until stopDriving is called");
     }
   }
   else if(turn_dir == 'l') { //if robot should turn left
-    maneuver(-200,200,1); //turn left
+    maneuver(-speed_time,speed_time,1); //turn left
     if (_debug){// if debug is on, print following:
       Serial.print("turn to the left until stopDriving is called");
     }
@@ -103,4 +99,27 @@ void Movement::stopDriving(){
   if (_debug){// if debug is on, print following:
     Serial.println("stopped with driving");
   }
+}
+
+void Movement::maneuver(int speed_left, int speed_right, int ms_time){
+  // speedLeft, speedRight ranges: Backward  Linear  Stop  Linear   Forward
+  //                               -200      -100......0......100       200
+  servo_left.writeMicroseconds(1500 + speed_left);   // Set Left servo speed
+  servo_right.writeMicroseconds(1500 - speed_right); // Set right servo speed
+  delay(ms_time);                                   // Delay for msTime
+}
+
+int Movement::findSpeedTime(int speed_factor){
+  // speed_factor between 0 and 10
+  if (speed_factor<0)
+  {
+    speed_factor = 0;
+  }
+  if (speed_factor>10)
+  {
+    speed_factor = 10;
+  }
+  int speed_time;
+  speed_time = map(speedfactor, 0, 10, 0, 200);
+  return speed_time;
 }
--- Libraries/Movement/Movement.h
+++ Libraries/Movement/Movement.h
@@ -7,26 +7,29 @@
 class Movement{
 	public:
     //constructor
-		Movement(int left_servo_pin = 12, int right_servo_pin = 13, bool debug=false);
+		Movement(int left_servo_pin = 13, int right_servo_pin = 12, bool debug=false);
 
     //public methods:
     void begin(int baudrate = 9600);
-		void moveStraight(int distance, char drive_dir);
-		void turn(int angle, char char_dir);
-    void driveInf(char drive_dir);
-    void turnInf(char turn_dir);
+		void moveStraight(int distance, char drive_dir, int speed_factor=10);
+		void turn(int angle, char char_dir, int speed_factor=10);
+    void driveInf(char drive_dir, int speed_factor=10);
+    void turnInf(char turn_dir, int speed_factor=10);
     void stopDriving();
 	  void maneuver(int speed_left, int speed_right, int ms_time);
-	
+
 	private:
     // private methods
 		Servo servo_left;
 		Servo servo_right;
-    
+
     //private variables
     int _left_servo_pin;
     int _right_servo_pin;
     int _debug;
+
+		//private functions
+		int findSpeedTime(int speed_factor);
 };
 
 #endif //Movement_h
