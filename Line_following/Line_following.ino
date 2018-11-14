4e989418a2b1680e819c47385154377f25545061
Kraak <b.r.kraak@student.tue.nl>
2018-11-14 17:06:33 +0100

    created findLeftIRAvg and findRightIRAvg functions

--- Line_following/Line_following.ino
+++ Line_following/Line_following.ino
@@ -13,37 +13,46 @@ void setup()
   move.begin(9600);
 }
 
-int i; int RightTot=0; int LeftTot=0; int LeftAvg=0; int RightAvg=0; int left_value; int right_value;
 void loop()
 {
-  
     while(1){
       move.driveInf('f'); 
-      for(i=0; i<10; i++){
-      left_value = analogRead(LEFT_SENSOR);
-      right_value = analogRead(RIGHT_SENSOR);
-      LeftTot=left_value+LeftTot;
-      RightTot=right_value+RightTot;
-      }
-    LeftAvg=LeftTot/10;
-    RightAvg=RightTot/10;
-    LeftTot=0;
-    RightTot=0;
+      int left_avg = findLeftIRAvg();
+      int right_avg = findRightIRAvg();
 
-    Serial.print("Left sensor value: ");
-    Serial.print(LeftAvg);
-    Serial.print("; right sensor value: ");
-    Serial.print(RightAvg);
-    Serial.println();
+      Serial.print("Left sensor value: ");
+      Serial.print(left_avg);
+      Serial.print("; right sensor value: ");
+      Serial.print(right_avg);
+      Serial.println();
     
-    if(LeftAvg>700){
-      move.turn(20,'l');}
-    else if(RightAvg>700){
-      move.turn(20,'r');}
+      if(left_avg>700){
+        move.turn(20,'l');
+        }
+      else if(right_avg>700){
+        move.turn(20,'r');
+        }
     }
-  
- 
+}
 
+int findLeftIRAvg(){
+  int left_tot = 0;
+  for(int i=0; i<10; i++){
+    int left_value = analogRead(LEFT_SENSOR);
+    left_tot=left_value+left_tot;
+  }
+  int left_avg = left_tot/10;
+  return left_avg;
+}
+
+int findRightIRAvg(){
+  int right_tot = 0;
+  for(int i=0; i<10; i++){
+    int right_value = analogRead(RIGHT_SENSOR);
+    right_tot=right_value+right_tot;
+  }
+  int right_avg = right_tot/10;
+  return right_avg;
 }
 
 
